1d46a3095ad3d401fe85c0b604095a641b0c0a93
Constantine <cmen.itis@gmail.com>
2016-12-08 09:54:37 +0300

    Win and lose notifications added

--- src/ru/kpfu/itis/influence/controllers/GameController.java
+++ src/ru/kpfu/itis/influence/controllers/GameController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,22 +24,81 @@ public class GameController implements Initializable {
     @FXML
     private Button btnSurrender;
 
-    public void surrender(ActionEvent actionEvent) {
+    @FXML
+    private Button btnTestWin;
 
+    @FXML
+    private Button btnTestLose;
+
+    @FXML
+    private Button btnWinNotificationOk;
+
+    @FXML
+    private Button btnLoseNotificationOk;
+
+
+    // Next three methods look like the same method with different parameter in getResource() and setTitle() methods
+    // I think they may be united into one notification method
+
+    public void winNotification (ActionEvent actionEvent) {
         try {
-            Stage stage = new Stage();
-            Parent root = FXMLLoader.load(getClass().getResource("../fxml/surrender.fxml"));
-            stage.setTitle("Подтверждение");
-            stage.setMinWidth(300);
-            stage.setMinHeight(150);
-            stage.setResizable(false);
-            stage.setScene(new Scene(root));
-            stage.initModality(Modality.WINDOW_MODAL);
-            stage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow());
-            stage.show();
+            Stage modalStage = new Stage();
+            Parent modalWindow = FXMLLoader.load(getClass().getResource("../fxml/win.fxml"));
+            modalStage.setTitle("Win");
+            modalStage.setMinWidth(300);
+            modalStage.setMinHeight(150);
+            modalStage.setResizable(false);
+            modalStage.setScene(new Scene(modalWindow));
+            modalStage.initModality(Modality.WINDOW_MODAL);
+            modalStage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+            modalStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void loseNotification(ActionEvent actionEvent) {
+        try {
+            Stage modalStage = new Stage();
+            Parent modalWindow = FXMLLoader.load(getClass().getResource("../fxml/lose.fxml"));
+            modalStage.setTitle("Lose");
+            modalStage.setMinHeight(150);
+            modalStage.setMinWidth(300);
+            modalStage.setResizable(false);
+            modalStage.setScene(new Scene(modalWindow));
+            modalStage.initModality(Modality.WINDOW_MODAL);
+            modalStage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow());
+            modalStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void surrender(ActionEvent actionEvent) {
+        try {
+            Stage modalStage = new Stage();
+            Parent modalWindow = FXMLLoader.load(getClass().getResource("../fxml/surrender.fxml"));
+            modalStage.setTitle("Подтверждение");
+            modalStage.setMinWidth(300);
+            modalStage.setMinHeight(150);
+            modalStage.setResizable(false);
+            modalStage.setScene(new Scene(modalWindow));
+            modalStage.initModality(Modality.WINDOW_MODAL);
+            modalStage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow());
+            modalStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void winEvent(ActionEvent actionEvent) {
+        Stage modalStage = (Stage) btnWinNotificationOk.getScene().getWindow();
+        modalStage.close();
+    }
+
+    public void loseEvent(ActionEvent actionEvent) {
+        Stage modalStage = (Stage) btnLoseNotificationOk.getScene().getWindow();
+        modalStage.close();
     }
 
     @Override
